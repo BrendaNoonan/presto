aea4a0ca221cb6667b68242d6d0acfc40f3fbce5
arhimondr <andriyrosa@gmail.com>
2016-02-12 21:23:33 +0100

    Do not bind HdfsAuthenticatingMetadata when hdfs authentication is not required

--- presto-hive/src/main/java/com/facebook/presto/hive/HiveClientModule.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/HiveClientModule.java
@@ -13,7 +13,6 @@
  */
 package com.facebook.presto.hive;
 
-import com.facebook.presto.hive.auth.HdfsAuthenticatingMetadata;
 import com.facebook.presto.hive.metastore.CachingHiveMetastore;
 import com.facebook.presto.hive.metastore.HiveMetastore;
 import com.facebook.presto.hive.orc.DwrfPageSourceFactory;
@@ -24,7 +23,6 @@ import com.facebook.presto.hive.parquet.ParquetPageSourceFactory;
 import com.facebook.presto.hive.parquet.ParquetRecordCursorProvider;
 import com.facebook.presto.hive.rcfile.RcFilePageSourceFactory;
 import com.facebook.presto.spi.ConnectorHandleResolver;
-import com.facebook.presto.spi.ConnectorMetadata;
 import com.facebook.presto.spi.ConnectorPageSinkProvider;
 import com.facebook.presto.spi.ConnectorPageSourceProvider;
 import com.facebook.presto.spi.ConnectorSplitManager;
@@ -32,7 +30,6 @@ import com.facebook.presto.spi.PageIndexerFactory;
 import com.facebook.presto.spi.type.TypeManager;
 import com.google.inject.Binder;
 import com.google.inject.Module;
-import com.google.inject.PrivateModule;
 import com.google.inject.Provides;
 import com.google.inject.Scopes;
 import com.google.inject.multibindings.Multibinder;
@@ -108,16 +105,6 @@ public class HiveClientModule
 
         binder.bind(HivePartitionManager.class).in(Scopes.SINGLETON);
         binder.bind(LocationService.class).to(HiveLocationService.class).in(Scopes.SINGLETON);
-        binder.install(new PrivateModule()
-        {
-            @Override
-            protected void configure()
-            {
-                bind(HiveMetadata.class).in(Scopes.SINGLETON);
-                bind(ConnectorMetadata.class).to(HdfsAuthenticatingMetadata.class).in(Scopes.SINGLETON);
-                expose(ConnectorMetadata.class);
-            }
-        });
         binder.bind(ConnectorSplitManager.class).to(HiveSplitManager.class).in(Scopes.SINGLETON);
         binder.bind(ConnectorPageSourceProvider.class).to(HivePageSourceProvider.class).in(Scopes.SINGLETON);
         binder.bind(ConnectorPageSinkProvider.class).to(HivePageSinkProvider.class).in(Scopes.SINGLETON);
